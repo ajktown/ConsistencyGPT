fb6d0d97c6e829a4668b8aba41ab3112297253c1
AJ Kim <jkim67cloud@gmail.com>
2024-01-21 12:13:20 +0900

    feat: simple loop of each day of 2023

--- src/components/organism_word_card_frame/index.tsx
+++ src/components/organism_word_card_frame/index.tsx
@@ -1,45 +1,28 @@
 import { FC } from 'react'
-import { Stack, Box } from '@mui/material'
-import NewWordBox from '../molecule_new_word_box'
-import WordCardsFrameRefreshButtonPart from '../atom_word_cards_frame_parts/index.refresh-button'
-import WordCardsFrameSurfingButtonPart from '../atom_word_cards_frame_parts/index.surfing-button'
-import WordCardsChunk from '../organism_word_card_chunk'
-import { WordCardFrameStyle } from './index.style'
-import WordCardDialog from '../molecule_word_card/index.dialog'
-import TagButtonChunkSemesters from '../molecule_tag_button_chunk/index.semesters'
-import TagButtonChunkDetailed from '../molecule_tag_button_chunk/index.detailed'
-import WordCardsFramePreferenceButtonPart from '../atom_word_cards_frame_parts/index.preference-button'
-import WordCardsFrameArchiveSwitchPart from '../atom_word_cards_frame_parts/index.archive-switch'
-import WordCardsFrameArchiveModePart from '../atom_word_cards_frame_parts/index.archive-mode'
-import SharedResourceDialog from '../dialog_shared_resource'
-import WordCardsFrameReviewModePart from '../atom_word_cards_frame_parts/index.review-mode'
+import { Stack } from '@mui/material'
+import ReactActivityCalendar, { Activity } from 'react-activity-calendar'
+
+const activities: Activity[] = []
+
+const startDate = new Date(`2024-01-01`)
+const endDate = new Date(`2024-12-31`)
+
+for (let d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
+  activities.push({
+    date: d.toISOString().split(`T`)[0],
+    count: 1, // Default value, replace with actual data
+    level: 0, // Default value, replace with actual data
+  })
+}
 
 const WordCardFrame: FC = () => {
   return (
     <Stack width="100%" alignItems="center">
-      <Stack {...WordCardFrameStyle}>
+      <Stack>
         {/* Header */}
-        <Stack direction="row" spacing={0.7} alignItems="center">
-          <Box flexGrow={1} />
-          <WordCardsFrameReviewModePart />
-          <WordCardsFrameArchiveSwitchPart />
-          <WordCardsFrameSurfingButtonPart />
-          <WordCardsFrameRefreshButtonPart />
-          <WordCardsFramePreferenceButtonPart />
-        </Stack>
-        <Stack alignItems={`center`} spacing={0.35}>
-          <TagButtonChunkSemesters />
-          <TagButtonChunkDetailed />
-        </Stack>
         {/* Body */}
-        <Stack spacing={0.5} alignItems="center">
-          <NewWordBox />
-          <WordCardsFrameArchiveModePart />
-          <WordCardsChunk />
-        </Stack>
+        <ReactActivityCalendar data={activities} />
         {/* Dialog */}
-        <WordCardDialog />
-        <SharedResourceDialog />
       </Stack>
     </Stack>
   )
