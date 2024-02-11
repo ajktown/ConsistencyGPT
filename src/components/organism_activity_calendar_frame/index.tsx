0642cfda49f2540b7b8e0f4111f19cbcfeeb691b
AJ Kim <jkim67cloud@gmail.com>
2024-02-11 11:22:31 +0900

    delete: test button

--- src/components/organism_activity_calendar_frame/index.tsx
+++ src/components/organism_activity_calendar_frame/index.tsx
@@ -1,20 +1,16 @@
 import { FC } from 'react'
 import { Stack } from '@mui/material'
 import ActivityCalendar from '@/components/molecule_activity_calendar'
-import StyledTextButtonAtom from '@/atoms/StyledTextButton'
+import RefreshButton from '../atom_refresh_button'
 
 const ActivityCalendarFrame: FC = () => {
   return (
     <Stack width="100%" alignItems="center">
       <Stack>
         {/* Header */}
+        <RefreshButton />
         {/* Body */}
         <ActivityCalendar />
-        <Stack>
-          <StyledTextButtonAtom
-            title="POST /api/v1/action"
-          />
-        </Stack>
         {/* Dialog */}
       </Stack>
     </Stack>
