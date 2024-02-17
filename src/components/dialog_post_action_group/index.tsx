a9975a6ffc42632c1640dab4a5662779cba77419
AJ Kim <jkim67cloud@gmail.com>
2024-02-17 16:48:34 +0900

    feat: warning

--- src/components/dialog_post_action_group/index.tsx
+++ src/components/dialog_post_action_group/index.tsx
@@ -1,10 +1,12 @@
 import StyledTextButtonAtom from '@/atoms/StyledTextButton'
 import StyledTextField from '@/atoms/StyledTextField'
+import StyledTextWithIconHead from '@/atoms/StyledTextWithPrefixButton'
 import { usePostActionGroup } from '@/hooks/action-group/use-post-action-group.hook'
 import StyledDialog from '@/organisms/StyledDialog'
 import { DialogActions, DialogContent, DialogTitle, Stack } from '@mui/material'
 import React, { useCallback, useState } from 'react'
-
+import WarningIcon from '@mui/icons-material/Warning'
+import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred'
 const PostActionGroupDialog = () => {
   const [isOpen, setOpen] = useState(false)
   const [task, setTask] = useState(``)
@@ -50,17 +52,62 @@ const PostActionGroupDialog = () => {
           <StyledTextField
             value={fromTime}
             onChange={setFromTime}
-            label={`From Time: Between 0 ~ 1439`}
+            label={`From Time Minutes: Between 0 ~ 1439`}
           />
           <StyledTextField
             value={toTime}
             onChange={setToTime}
-            label={`Until Time: 1~1440`}
+            label={`Until Time Minutes: 1~1440`}
+          />
+          <StyledTextWithIconHead
+            prefixIcon={<ReportGmailerrorredIcon fontSize="small" />}
+            textProps={{
+              fontFamily: `Cormorant Garamond`,
+              variant: `caption`,
+              fontStyle: `italic`,
+            }}
+            title={`AJK Town knows the UI is not ready yet. If FromTime == 240 (4am). If FromTime == 1200 (8pm).`}
+          />
+          <StyledTextWithIconHead
+            prefixIcon={<ReportGmailerrorredIcon fontSize="small" />}
+            textProps={{
+              fontFamily: `Cormorant Garamond`,
+              variant: `caption`,
+              fontStyle: `italic`,
+            }}
+            title={`Sorry for the inconvenience. The time picker is coming up soon!`}
+          />
+          <StyledTextWithIconHead
+            prefixIcon={<ReportGmailerrorredIcon fontSize="small" />}
+            textProps={{
+              fontFamily: `Cormorant Garamond`,
+              variant: `caption`,
+              fontStyle: `italic`,
+            }}
+            title={`fromTime must be smaller than toTime.`}
           />
         </Stack>
       </DialogContent>
       <DialogActions>
-        <StyledTextButtonAtom title="Add" onClick={onPost} />
+        <StyledTextWithIconHead
+          prefixIcon={<WarningIcon color="warning" fontSize="small" />}
+          textProps={{
+            fontFamily: `Cormorant Garamond`,
+            variant: `caption`,
+            fontStyle: `italic`,
+          }}
+          title={`Be careful. You cannot edit consistency after adding.`}
+        />
+        <StyledTextButtonAtom
+          title="Add"
+          onClick={onPost}
+          isDisabled={
+            task.trim().length === 0 ||
+            isNaN(parseInt(fromTime)) ||
+            isNaN(parseInt(toTime)) ||
+            parseInt(toTime) <= parseInt(fromTime)
+          }
+        />
       </DialogActions>
     </StyledDialog>
   )
