aa0819d552b517225dd18ddad2b9662b35cc1328
AJ Kim <jkim67cloud@gmail.com>
2024-02-17 14:12:01 +0900

    fix: ok

--- src/components/molecule_action_group_card/index.special-message.tsx
+++ src/components/molecule_action_group_card/index.special-message.tsx
@@ -1,5 +1,7 @@
 import { ActionGroupFixedId } from '@/constants/action-group.constant'
+import { envLambda } from '@/lambdas/get-env.lambda'
 import { actionGroupFamily } from '@/recoil/action-groups/action-groups.state'
+import { Stack, Typography } from '@mui/material'
 import { FC, Fragment } from 'react'
 import { useRecoilValue } from 'recoil'
 
@@ -12,7 +14,23 @@ const ActionGroupCardSpecialMessage: FC<Props> = ({ id }) => {
   if (actionGroup?.props.id !== ActionGroupFixedId.DailyPostWordChallenge)
     return null
 
-  return <Fragment>{`hi`}</Fragment>
+  if (actionGroup.isTodayHandled)
+    return (
+      <Typography fontFamily={`Cormorant Garamond`}>
+        {`You have accomplished posting a daily word. Keep up the good work!`}
+      </Typography>
+    )
+
+  return (
+    <Stack mt={1}>
+      <Typography color="warning" fontFamily={`Cormorant Garamond`}>
+        {`It seems like you have not added your daily word yet. Consistency is the key in your success.`}
+      </Typography>
+      <Typography fontFamily={`Cormorant Garamond`}>
+        {`Please go visit ${envLambda.getWordnoteUrl()} and add a word for today.`}
+      </Typography>
+    </Stack>
+  )
 }
 
 export default ActionGroupCardSpecialMessage
