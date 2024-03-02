184d2acb758bdfc81112b23ffcfc95bc40ab841d
AJ Kim <jkim67cloud@gmail.com>
2024-03-02 15:00:58 +0900

    feat: ok

--- src/pages/users/[nickname].tsx
+++ src/pages/users/[nickname].tsx
@@ -1,19 +1,31 @@
 import { useRouter } from 'next/router'
-import { FC } from 'react'
+import { FC, useCallback } from 'react'
 import ErrorApiConnectionFail from '@/components/molecule_error_api_connection_fail'
 import RitualsFrame from '@/components/organism_rituals_frame'
 import Appbar from '@/components/organism_appbar'
 import StyledCentered from '@/organisms/StyledCentered'
+import StyledTextButtonAtom from '@/atoms/StyledTextButton'
 import { Typography } from '@mui/material'
 
 const UserByNicknamePage: FC = () => {
   const router = useRouter()
   const { nickname } = router.query
 
-  if (typeof nickname !== `string` || !nickname.trim())
+  const onClickToAdminProfile = useCallback(() => {
+    router.push(`/users/mlajkim`)
+  }, [router])
+
+  // This never happens because it will be considered as /users/index.tsx (for ts sake only)
+  if (typeof nickname !== `string` || !nickname.trim()) return null
+
+  if (nickname !== `mlajkim`)
     return (
       <StyledCentered>
-        <Typography>{`User must be specified`}</Typography>
+        <Typography>{`Currently user "${nickname}" is not supported`}</Typography>
+        <StyledTextButtonAtom
+          title="To mlajkim's profile"
+          onClick={onClickToAdminProfile}
+        />
       </StyledCentered>
     )
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,11 +1,22 @@
-import { FC } from 'react'
+import { FC, useCallback } from 'react'
 import StyledCentered from '@/organisms/StyledCentered'
 import { Typography } from '@mui/material'
-
+import StyledTextButtonAtom from '@/atoms/StyledTextButton'
+import { useRouter } from 'next/router'
 const UsersPage: FC = () => {
+  const router = useRouter()
+
+  const onClickToAdminProfile = useCallback(() => {
+    router.push(`/users/mlajkim`)
+  }, [router])
+
   return (
     <StyledCentered>
       <Typography>{`User must be specified`}</Typography>
+      <StyledTextButtonAtom
+        title="To mlajkim's profile"
+        onClick={onClickToAdminProfile}
+      />
     </StyledCentered>
   )
 }
