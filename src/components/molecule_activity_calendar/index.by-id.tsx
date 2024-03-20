608dffc85f98f6d54cacc4b1aadbeda3f354f1fa
AJ Kim <jkim67cloud@gmail.com>
2024-03-20 17:43:44 +0900

    doc: a bit more

--- src/components/molecule_activity_calendar/index.by-id.tsx
+++ src/components/molecule_activity_calendar/index.by-id.tsx
@@ -1,16 +1,38 @@
 import { actionGroupFamily } from '@/recoil/action-groups/action-groups.state'
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
 import ReactActivityCalendar from 'react-activity-calendar'
 import { useRecoilValue } from 'recoil'
 import ActivityCalendarUnknown from './index.unknown'
 import { ActionGroupFixedId } from '@/constants/action-group.constant'
+import useWindowSize from 'react-use/lib/useWindowSize'
 
 // TODO: The post consistency will use the same here
 interface Props {
   id: string
 }
+
+const SHRINKING_MIN_WIDTH = 920 // starting width that makes the calendar shrink
+const COMMIT_BLOCK_WIDTH = 16 // 12 x 12 with 2 indent between
+
 const ActivityCalendarById: FC<Props> = ({ id }) => {
+  const { width } = useWindowSize()
   const actionGroup = useRecoilValue(actionGroupFamily(id))
+
+  // sliceFrom contains number of days to slice from the start based on window width
+  // This adjusts the calendar display for smaller screens by removing blocks
+  // This is based on 365 days display
+  const [sliceFrom, totalCount] = useMemo(() => {
+    // const widthToBeShrink = Math.max(0, SHRINKING_MIN_WIDTH - width)
+    // => if width is big enough, it will have 0 slices
+    // const weeksToSlice = Math.ceil(Math.max(0, SHRINKING_MIN_WIDTH - width) / COMMIT_BLOCK_WIDTH)
+    // => how many weeks to slice
+    // const daysToSlice = Math.ceil(Math.max(0, SHRINKING_MIN_WIDTH - width) / COMMIT_BLOCK_WIDTH) * 7
+    // => how many days to slice
+    const sliceFrom =
+      Math.ceil(Math.max(0, SHRINKING_MIN_WIDTH - width) / COMMIT_BLOCK_WIDTH) *
+      7 // days
+    return [sliceFrom, 365 - sliceFrom]
+  }, [width])
 
   if (actionGroup === null) return <ActivityCalendarUnknown />
   if (actionGroup === undefined) return null
@@ -28,8 +50,8 @@ const ActivityCalendarById: FC<Props> = ({ id }) => {
         light: [`#d4e6cf`, `#a7d4bf`, `#80d1ab`, `#56d197`, `#29cc7f`],
         dark: [`#d4e6cf`, `#a7d4bf`, `#80d1ab`, `#56d197`, `#29cc7f`],
       }}
-      totalCount={actionGroup.totalCount}
-      data={actionGroup.actions.map((p) => {
+      totalCount={totalCount}
+      data={actionGroup.actions.slice(sliceFrom, 365).map((p) => {
         return {
           date: p.yyyymmdd,
           count: 1,
