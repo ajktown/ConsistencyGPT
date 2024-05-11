3225d04172f602a55a6e9589fefa0f17e7faf957
AJ Kim <jkim67cloud@gmail.com>
2024-05-11 15:04:34 +0900

    feat: dummy action

--- src/api/action-groups/post-action-by-action-group-id.api.ts
+++ src/api/action-groups/post-action-by-action-group-id.api.ts
@@ -2,10 +2,15 @@ import axios from 'axios'
 import { CustomizedAxiosResponse } from '../index.interface'
 import { GetActionGroupRes } from './index.interface'
 
+export interface PostActionBodyDTO {
+  isDummy: boolean
+}
+
 export const postActionByActionGroupIdApi = async (
   actionGroupId: string,
+  dto: undefined | PostActionBodyDTO,
 ): Promise<CustomizedAxiosResponse<GetActionGroupRes>> => {
   const url = `/v1/action-groups/${actionGroupId}/actions`
-  const res = await axios.post(url)
+  const res = await axios.post(url, dto)
   return [res.data, res]
 }
