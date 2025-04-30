8df983b1acdd88a9d35d11357062a412387ceaac
Aaron Jeongwoo Kim <53258958+mlajkim@users.noreply.github.com>
2025-05-01 07:18:23 +0900

    Feat: Action Group Card Only Page (#93)
    # Background
    Build a basic page specifically for certain action group
    
    ## What's done?
    Implement a new page `/action-groups/{id}`
    
    ## What are the related PRs?
    None
    
    ## What's next?
    - Design better
    - ActionGroupCard with the link
    
    ## Checklist Before PR Review
    - [x] The following has been handled:
      - `Title` is checked
      - `Background` is filled
      - `What's done?` is filled
      - `What are the related PRs?` is filled
      - `What's next?` is filled
      - `Draft` is set for this PR
      - `Assignee` is set
      - `Labels` are set
      - `Development` is linked if related issue exists
    
    ## Checklist (Right Before PR Review Request)
    - [x] The following has been handled:
      - Final Operation Check is done
      - Mobile View Operation Check is done, if frontend related
      - Make this PR as an open PR
      - Double-check `What's done?` matches to the actual changes
      - Appropriate ajktown/docs sync pr done if needed
    
    ## Checklist (Reviewers)
    - [x] Review if the following has been handled:
      - Review Code
      - `Checklist Before PR Review` is correctly handled
      - `Checklist (Right Before PR Review Request)` is correctly handled
    - Check if there are any other missing TODOs (Checklists) that are not
    yet listed
      - Check if issue under `Development` is fully handled if exists
      - Check if appropriate issues are created from `What's next?`

---
+++ src/pages/action_groups/[id].tsx
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router'
+import { FC } from 'react'
+import ErrorApiConnectionFail from '@/components/molecule_error_api_connection_fail'
+import Appbar from '@/components/organism_appbar'
+import ActionGroupCard from '@/components/molecule_action_group_card'
+
+const ActionGroupByIdPage: FC = () => {
+  const router = useRouter()
+  const id = typeof router.query.id === `string` ? router.query.id : ``
+
+  return (
+    <Appbar>
+      <ErrorApiConnectionFail />
+      <ActionGroupCard id={id} />
+    </Appbar>
+  )
+}
+
+export default ActionGroupByIdPage
