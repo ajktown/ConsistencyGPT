01f735db1bd938dbd35493510b1cfa3eda1d0c50
AJ Kim <jkim67cloud@gmail.com>
2025-05-08 05:44:50 +0900

    comment

--- src/hooks/use-open-new-tab.ts
+++ src/hooks/use-open-new-tab.ts
@@ -1,7 +1,9 @@
 import { useCallback } from 'react'
 
 /** Returns onClick that will open a new tab of your browser with the given link */
-// TODO: If i do not provide header, what happens?
+// useOpenNewTab also handles path for "/hello/world" , and this will
+// open a new tab with the link "http://localhost:3000/hello/world" if you are
+// running the app on localhost:3000 (or any other host)
 export const useOpenNewTab = (link: string) => {
   const onOpenNewTab = useCallback(() => {
     const win = window.open(link, `_blank`)
