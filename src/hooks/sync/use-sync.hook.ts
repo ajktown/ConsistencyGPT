7cc83c677cdc767810834f476ac912ff9af9f519
AJ Kim <jkim67cloud@gmail.com>
2025-07-20 11:36:08 +0900

    feat

--- src/hooks/sync/use-sync.hook.ts
+++ src/hooks/sync/use-sync.hook.ts
@@ -4,22 +4,25 @@ import { usePreference } from '../preference/use-preference.hook'
 // Initializes preference state on component mount
 // This hook should not be used in multiple places simultaneously because it relies on shared state.
 // Using it in multiple places could lead to race conditions or inconsistent state updates.
-let isSyncInUse = false; // Tracks whether the hook is already in use
+// And therefore it is designed to be used in a single component or context where preference state is managed.
+let isSyncInUse = false // Tracks whether the hook is already in use
 
 export const useSync = (): void => {
-  if (isSyncInUse) {
-    throw new Error('useSync hook is already in use. Avoid using it in multiple components simultaneously.');
-  }
-  isSyncInUse = true;
+  if (isSyncInUse)
+    throw new Error(
+      `useSync hook is already in use. Avoid using it in multiple components simultaneously.`,
+    )
+  isSyncInUse = true
 
-  const onGetPreference = usePreference();
+  const onGetPreference = usePreference()
 
   useEffect(() => {
     // update preference state by default:
-    onGetPreference();
+    onGetPreference()
 
+    // Cleanup:
     return () => {
-      isSyncInUse = false; // Reset the flag when the component unmounts
-    };
-  }, [onGetPreference]);
+      isSyncInUse = false // Reset the flag when the component unmounts
+    }
+  }, [onGetPreference])
 }
